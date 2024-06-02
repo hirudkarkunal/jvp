2e9d2a5984e132db9de95f265cc9f025c7f76693
Kunal Hirudkar <hirudkarkunal@outlook.com>
2024-06-02 16:55:23 +0530

    file add downloaded from spring initializer

---
+++ jvp/src/main/java/com/volunteerjvp/jvp/JvpvolunteerappApplication.java
@@ -0,0 +1,13 @@
+package com.volunteerjvp.jvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvpvolunteerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvpvolunteerappApplication.class, args);
+	}
+
+}
